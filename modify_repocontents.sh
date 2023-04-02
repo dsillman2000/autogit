4c9283d1b700b4050fe6c66a1fb5aa92a225782d
dsillman2000 <dsillman2000@gmail.com>
2023-04-02 13:12:41 -0700

    modify_repocontents

--- modify_repocontents.sh
+++ modify_repocontents.sh
@@ -1,36 +1,39 @@
-cd /home/dsillman/autogit
+cd /home/dsillman2000/autogit/
 NUM_ACTIONS=4
 
-for i in $NUM_ACTIONS; do
+for i in $(seq 1 1 $NUM_ACTIONS); do
 
-    ACTION=$(( $RANDOM % 4 ))
+    ACTION="$(( $RANDOM % 4 ))"
     RANDSEED="$(date)$RANDOM"
+
+    readarray -t CURRENT_FILENAMES < <(ls -1 ./repocontents)
+    if [[ -z $CURRENT_FILENAMES ]]; then
+        echo "Must add file";
+        ACTION="0"
+    fi
 
     case $ACTION in
         0) # add new file
-            NEW_FILENAME="./repocontents/$(echo "$RANDSEED" | md5sum)"
-            echo "$(date +%s)" >> $NEW_FIELNAME
+            NEW_FILENAME="./repocontents/$(echo "$RANDSEED" | md5sum | cut -d " " -f1)"
+            echo "$(date +%s)" >> $NEW_FILENAME
             ;;
         1) # remove existing file
-            readarray -t CURRENT_FILENAMES < <(ls -1 ./autogit/repocontents)
             SELECTED_FILENAME=${CURRENT_FILENAMES[ $RANDOM % ${#CURRENT_FILENAMES[@]} ]}
             rm "./repocontents/$SELECTED_FILENAME"
             ;;
         2) # append to existing file
-            readarray -t CURRENT_FILENAMES < <(ls -1 ./repocontents)
             SELECTED_FILENAME=${CURRENT_FILENAMES[ $RANDOM % ${#CURRENT_FILENAMES[@]} ]}
             echo "$(date +%s)" >> "/home/dsillman2000/autogit/repocontents/$SELECTED_FILENAME"
             ;;
         3) # replace contents of existing file
-            readarray -t CURRENT_FILENAMES < <(ls -1 ./repocontents)
             SELECTED_FILENAME=${CURRENT_FILENAMES[ $RANDOM % ${#CURRENT_FILENAMES[@]} ]}
-            echo : > "./repocontents/$SELECTED_FILENAME"
+            rm "./repocontents/$SELECTED_FILENAME"
             echo "$(date +%s)" >> "./repocontents/$SELECTED_FILENAME"
             ;;
     esac
 
-    git add ./repocontents
-    git commit -m "$1"
-    git push
-
 done
+
+git add ./repocontents
+git commit -m "$1"
+git push
